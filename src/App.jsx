45fa9210276afe68798ab26c7b563290c18b65ad
Makktu <johnmcna@gmail.com>
2023-02-24 19:46:52 +0000

    fooling around with it

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const App = () => {
-  const [resourceType, setResourceType] = useState("posts");
-  const [items, setItems] = useState([]);
+export default function App() {
+  // const [resourceType, setResourceType] = useState("posts");
+  // const [items, setItems] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setItems(json));
+  // }, []);
+
+  // const buttonClickHandler = (type) => {
+  //   setResourceType(type);
+  // };
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+    setWindowWidth(window.innerHeight);
+  };
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }),
-    [resourceType];
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window, removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-  return <></>;
-};
-
-export default App;
-
-{
-  /* <div>
-<button onClick={() => setResourceType("posts")}>Posts</button>
-<button onClick={() => setResourceType("users")}>Users</button>
-<button onClick={() => setResourceType("comments")}>Comments</button>
-</div>
-<h1>{resourceType}</h1>
-{items.map((item, ind) => {
-item.key = ind;
-return <pre>{JSON.stringify(item)}</pre>;
-})} */
+  return (
+    <>
+      {/* <div>
+        <button onClick={() => buttonClickHandler("Posts")}>Posts</button>
+        <button onClick={() => buttonClickHandler("Users")}>Users</button>
+        <button onClick={() => buttonClickHandler("Comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return <pre key={item.id}>{JSON.stringify(item)}</pre>;
+      })} */}
+      {/* <div>Height: {windowHeight}</div> */}
+      <div>Width: {windowWidth}</div>
+    </>
+  );
 }
